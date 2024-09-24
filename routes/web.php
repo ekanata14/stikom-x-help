9d3b5ddb82b7acb158fd2b2bfd9e0e7946036add
ekanata14 <ekanata1411@gmail.com>
2024-09-24 20:32:51 +0800

    feat: setup ui component with template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,13 +22,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/users', function () {
+    return view('admin-dashboard.users.index');
+})->middleware(['auth', 'verified'])->name('users');
+
 Route::get('/admin', function () {
-    return view('admin.users.index');
+    return view('admin-dashboard.admin.index');
 })->middleware(['auth', 'verified'])->name('admin');
 
-Route::get('/users', function () {
-    return view('admin.users.index');
-})->middleware(['auth', 'verified'])->name('users');
+Route::get('/user-types', function () {
+    return view('admin-dashboard.user-types.index');
+})->middleware(['auth', 'verified'])->name('user-types');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -25,4 +40,18 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+// useless routes
+// Just to demo sidebar dropdown links active states.
+Route::get('/buttons/text', function () {
+    return view('buttons-showcase.text');
+})->middleware(['auth'])->name('buttons.text');
+
+Route::get('/buttons/icon', function () {
+    return view('buttons-showcase.icon');
+})->middleware(['auth'])->name('buttons.icon');
+
+Route::get('/buttons/text-icon', function () {
+    return view('buttons-showcase.text-icon');
+})->middleware(['auth'])->name('buttons.text-icon');
+
+require __DIR__ . '/auth.php';

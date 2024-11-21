7c5ec05e28f2697769f22c130a1c7756ebe4eb34
ekanata14 <ekanata1411@gmail.com>
2024-11-21 14:44:31 +0800

    feat: add data detail on dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -17,8 +17,12 @@ class DashboardController extends Controller
             'activePage' => 'dashboard',
             'totalUsers' => User::where('id_user_type', '!=', '1')->count(),
             'totalPurchases' => Purchase::count(),
+            'totalVerifiedPurchases' => Purchase::where('status', 'paid')->count(),
+            'totalUnverifiedPurchases' => Purchase::where('status', 'pending')->count(),
             'totalIncomeIDR' => Purchase::where('currency', 'IDR')->sum('total_price'),
+            'totalVerifiedIncomeIDR' => Purchase::where('currency', 'IDR')->where('status', 'paid')->sum('total_price'),
             'totalIncomeUSD' => Purchase::where('currency', 'USD')->sum('total_price'),
+            'totalVerifiedIncomeUSD' => Purchase::where('currency', 'USD')->where('status', 'paid')->sum('total_price'),
         ];
 
         return view('admin-dashboard.dashboard', $viewData);
--- resources/views/admin-dashboard/dashboard.blade.php
+++ resources/views/admin-dashboard/dashboard.blade.php
@@ -15,7 +15,7 @@
     <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
         {{ __('Welcome, ') }}{{ auth()->user()->complete_name }}
     </div>
-    <div class="grid grid-cols-4 px-20 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 mt-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 px-20 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 mt-8">
         {{-- card-start --}}
         <div
             class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
@@ -29,6 +29,8 @@
             class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
             <div class="p-5">
                 Total Purchase: {{ $totalPurchases }}
+                <br>
+                Total Verified Purchase: {{ $totalVerifiedPurchases }}
             </div>
         </div>
         {{-- card-end --}}
@@ -38,6 +40,9 @@
             <div class="p-5">
                 Total Income IDR:
                 {{ number_format($totalIncomeIDR, 0, ',', '.') }}
+                <br>
+                Verified Income IDR:
+                {{ number_format($totalVerifiedIncomeIDR, 0, ',', '.') }}
             </div>
         </div>
         {{-- card-end --}}
@@ -47,6 +52,9 @@
             <div class="p-5">
                 Total Income USD:
                 {{ number_format($totalIncomeUSD, 0, ',', '.') }}
+                <br>
+                Verified Income USD:
+                {{ number_format($totalVerifiedIncomeUSD, 0, ',', '.') }}
             </div>
         </div>
         {{-- card-end --}}

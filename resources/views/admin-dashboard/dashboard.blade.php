aff6892373f2efa91fe3389bc1bf8369d0af81ad
ekanata14 <ekanata1411@gmail.com>
2024-11-21 15:09:27 +0800

    feat: update dashboard layout

--- resources/views/admin-dashboard/dashboard.blade.php
+++ resources/views/admin-dashboard/dashboard.blade.php
@@ -16,78 +16,78 @@
         {{ __('Welcome, ') }}{{ auth()->user()->complete_name }}
     </div>
     <div
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-20 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 mt-8">
-        {{-- card-start --}}
-        <div
-            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
-            <div class="p-5">
-                Total Users: {{ $totalUsers }}
+        class="grid grid-cols-1 md:grid-cols-2 px-20 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 mt-8">
+        <div>
+            {{-- card-start --}}
+            <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
+                <div class="p-5">
+                    Total Users: {{ $totalUsers }}
+                </div>
+            </div>
+            {{-- card-end --}}
+            {{-- card-start --}}
+            <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
+                <div class="p-5">
+                    Total Purchase: {{ $totalPurchases }}
+                    <br>
+                    Total Verified Purchase: {{ $totalVerifiedPurchases }}
+                </div>
+            </div>
+            {{-- card-end --}}
+            {{-- card-start --}}
+            <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
+                <div class="p-5">
+                    Total Income IDR:
+                    {{ number_format($totalIncomeIDR, 0, ',', '.') }}
+                    <br>
+                    Verified Income IDR:
+                    {{ number_format($totalVerifiedIncomeIDR, 0, ',', '.') }}
+                </div>
+            </div>
+            {{-- card-end --}}
+            {{-- card-start --}}
+            <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
+                <div class="p-5">
+                    Total Income USD:
+                    {{ number_format($totalIncomeUSD, 0, ',', '.') }}
+                    <br>
+                    Verified Income USD:
+                    {{ number_format($totalVerifiedIncomeUSD, 0, ',', '.') }}
+                </div>
             </div>
         </div>
-        {{-- card-end --}}
-        {{-- card-start --}}
-        <div
-            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
-            <div class="p-5">
-                Total Purchase: {{ $totalPurchases }}
-                <br>
-                Total Verified Purchase: {{ $totalVerifiedPurchases }}
-            </div>
-        </div>
-        {{-- card-end --}}
-        {{-- card-start --}}
-        <div
-            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
-            <div class="p-5">
-                Total Income IDR:
-                {{ number_format($totalIncomeIDR, 0, ',', '.') }}
-                <br>
-                Verified Income IDR:
-                {{ number_format($totalVerifiedIncomeIDR, 0, ',', '.') }}
-            </div>
-        </div>
-        {{-- card-end --}}
-        {{-- card-start --}}
-        <div
-            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
-            <div class="p-5">
-                Total Income USD:
-                {{ number_format($totalIncomeUSD, 0, ',', '.') }}
-                <br>
-                Verified Income USD:
-                {{ number_format($totalVerifiedIncomeUSD, 0, ',', '.') }}
-            </div>
-        </div>
-    </div>
-    <div class="grid grid-cols-1 px-20 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 mt-8">
-        <div
-            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
-            <table>
-                <thead>
-                    <tr>
-                        <th>User Type</th>
-                        <th>Total</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($totalUserBasedOnType as $userType)
+        <div>
+            <div
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center">
+                <table>
+                    <thead>
                         <tr>
-                            <td>{{ $userType->type_name }}</td>
-                            <td>{{ $userType->total_user }}</td>
-                            <td>
-                                <a href="{{ route('user-types.detail', $userType->id) }}">
-                                    <x-button variant="info">
-                                        {{ __('Detail') }}
-                                    </x-button>
-                                </a>
-                            </td>
+                            <th>User Type</th>
+                            <th>Total</th>
+                            <th>Action</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($totalUserBasedOnType as $userType)
+                            <tr>
+                                <td>{{ $userType->type_name }}</td>
+                                <td>{{ $userType->total_user }}</td>
+                                <td>
+                                    <a href="{{ route('user-types.detail', $userType->id) }}">
+                                        <x-button variant="info">
+                                            {{ __('Detail') }}
+                                        </x-button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-        {{-- card-end --}}
-    </div>
     </div>
 </x-app-layout>

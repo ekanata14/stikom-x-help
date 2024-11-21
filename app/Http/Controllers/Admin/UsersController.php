4bfb371adb8a796a40f4dd2c6d5aae1f69f1108c
ekanata14 <ekanata1411@gmail.com>
2024-11-21 18:16:28 +0800

    feat: remove return  at complete profile email

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -60,7 +60,6 @@ class UsersController extends Controller
     public function completeProfileEmail()
     {
         $users = User::where('id_user_type', '=', '8')->get();
-        return $users;
 
         foreach ($users as $user) {
             MailHistory::create([

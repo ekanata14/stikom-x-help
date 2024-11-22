e7c2cd645d3d668df0e26e969f494228ea15186a
ekanata14 <ekanata1411@gmail.com>
2024-11-23 05:49:40 +0800

    fix: change variable

--- app/Http/Controllers/Admin/CheckInController.php
+++ app/Http/Controllers/Admin/CheckInController.php
@@ -50,7 +50,7 @@ class CheckInController extends Controller
         $user = Purchase::where('status', '=' , 'paid')->get();
         foreach ($user as $u) {
             MailHistory::create([
-                'user_id' => $user->user_id,
+                'user_id' => $u->user_id,
             ]);
             Mail::to($u->user->email)->send(new QrCodeMail($u->user_id));
         }

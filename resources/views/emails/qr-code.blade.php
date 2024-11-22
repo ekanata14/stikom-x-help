d4707d6a6ad44b3946e9005150bf621c99138253
ekanata14 <ekanata1411@gmail.com>
2024-11-23 05:41:46 +0800

    feat: change see details to login on button

--- resources/views/emails/qr-code.blade.php
+++ resources/views/emails/qr-code.blade.php
@@ -203,7 +203,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
 <div align="center">
   <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://unlayer.com" style="height:37px; v-text-anchor:middle; width:180px;" arcsize="11%"  stroke="f" fillcolor="#0202aa"><w:anchorlock/><center style="color:#FFFFFF;"><![endif]-->
     <a href="https://esgbali.org/login" target="_blank" class="v-button v-size-width v-font-size" style="box-sizing: border-box;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #0202aa; border-radius: 4px;-webkit-border-radius: 4px; -moz-border-radius: 4px; width:31%; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 14px;">
-      <span style="display:block;padding:10px 20px;line-height:120%;">See Details</span>
+      <span style="display:block;padding:10px 20px;line-height:120%;">Login</span>
     </a>
     <!--[if mso]></center></v:roundrect><![endif]-->
 </div>
